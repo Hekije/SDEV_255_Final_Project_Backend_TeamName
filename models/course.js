4a59a909c8fd3db3d8637fbfd4880773ac5db5a1
shorban1 <shorban1@ivytech.edu>
2025-02-24 21:56:55 -0500

    Altered the course model

--- models/course.js
+++ models/course.js
@@ -2,7 +2,9 @@ const db = require("../db");
 
 const Course = db.model("Course", {
   title: { type: String, required: true },
-  code: { type: String, required: true },
+  department: { type: String, required: true },
+  course_number: { type: Number, required: true },
+  credits: { type: Number, required: true },
   description: { String },
   instructor_ids: [String],
   student_ids: [String],
