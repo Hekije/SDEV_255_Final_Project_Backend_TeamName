b142c7adb232c26f9075d8b8b666f3d3dc2fd390
shorban1 <shorban1@ivytech.edu>
2025-03-03 14:46:27 -0500

    Added backend authorization

--- app.js
+++ app.js
@@ -3,12 +3,71 @@ const express = require("express");
 var cors = require("cors");
 //activate
 const bodyParser = require("body-parser");
+const jwt = require("jwt-simple");
 const Course = require("./models/course");
+const User = require("./models/user");
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 const router = express.Router();
+
+//creating a new user
+router.post("/users", async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send("Missing username or password");
+  }
+
+  const newUser = await new User({
+    uid: req.body.uid,
+    username: req.body.username,
+    password: req.body.password,
+    role: req.body.role,
+    courses: req.body.courses,
+  });
+  try {
+    await newUser.save();
+    console.log(newUser);
+    res.status(201).send("User created");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.post("/auth", async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send("Missing username or password");
+    return;
+  }
+  let user = await User.findOne({ username: req.body.username });
+  if (!user) {
+    res.status(401).json({ error: "Bad Username" });
+  } else if (user.password != req.body.password) {
+    res.status(401).send("Bad password");
+  } else {
+    username2 = user.username;
+    const token = jwt.encode({ username: user.username }, secret);
+    const auth = 1;
+
+    res.json({ username2, token: token, auth: auth });
+  }
+});
+
+router.get("/users", async (req, res) => {
+  if (!req.headers["x-auth"]) {
+    return res.status(401).json({ error: "Missing X-Auth" });
+  }
+
+  const token = req.headers["x-auth"];
+  try {
+    const decoded = jwt.decode(token, secret);
+
+    let users = User.find({}, "username status");
+    res.json(users);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 //get all courses
 router.get("/courses", async (req, res) => {
